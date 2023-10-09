c80d18ca5bfded89e6d6617dc686bc7fe99ca1ce
David Márquez Mínguez <davidmarquezminguez@gmail.com>
2023-10-09 19:03:31 +0200

    Randomize pieces

--- main.js
+++ main.js
@@ -30,13 +30,40 @@ const BOARD = [
   [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
 ];
 
-const piece = {
-  position: { x: 4, y: 5 },
-  shape: [
+const shapes = [
+  [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+  ],
+  [
     [1, 1],
     [1, 1]
   ],
+  [
+    [1],
+    [1],
+    [1],
+    [1]
+  ],
+  [
+    [1, 1, 0],
+    [0, 1, 1]
+  ]
+];
+
+const piece = {
+  position: { x: 4, y: 5 },
+  shape: [
+    [1, 0],
+    [1, 0],
+    [1, 1]
+  ],
 };
+
+const bottomLimit = piece.position.y < BOARD_HEIGHT - piece.shape.length;
+const rightLimit = piece.position.x < BOARD_WIDTH - piece.shape[0].length;
+const leftLimit = piece.position.x > 0;
 
 canvas.width = BOARD_WIDTH * BLOCK_SIZE;
 canvas.height = BOARD_HEIGHT * BLOCK_SIZE;
@@ -47,13 +74,16 @@ async function update() {
   drawBoard();
   drawPiece();
 
-  piecePlacement();
-  cleanRow();
+  handlePiecePlacement();
+  clearRow();
 
   window.requestAnimationFrame(update);
   //setTimeout(() => { window.requestAnimationFrame(update); }, 500);
 }
 
+/**
+ * Draws the game board.
+ */
 const drawBoard = () => {
   context.fillStyle = 'black';
   context.fillRect(0, 0, canvas.width, canvas.height);
@@ -68,6 +98,9 @@ const drawBoard = () => {
   });
 };
 
+/**
+ * Draws the current piece on the game board.
+ */
 const drawPiece = () => {
   piece.shape.forEach((row, y) => {
     row.forEach((block, x) => {
@@ -79,6 +112,11 @@ const drawPiece = () => {
   });
 };
 
+/**
+ * Checks if a given move is valid for the current piece.
+ * @param {string} move - The move to be validated.
+ * @returns {boolean} True if the move is valid; otherwise, false.
+ */
 const isValidMove = (move) => {
   let isValid = true;
   if (move === 'ArrowLeft') {
@@ -89,6 +127,7 @@ const isValidMove = (move) => {
     }
   } else if (move === 'ArrowRight') {
     for (let row = piece.position.y; row < piece.position.y + piece.shape.length; row++) {
+      //piece.shape[0].reduce((accumulator, currentValue) => accumulator + currentValue, 0)] -> possible solution
       if (BOARD[row][piece.position.x + piece.shape[0].length] === 1) {
         isValid = false;
       }
@@ -104,33 +143,50 @@ const isValidMove = (move) => {
   return isValid;
 };
 
-const piecePlacement = () => {
+/**
+ * Handles the placement of the Tetris piece on the game board:
+ * - Checks for collisions and drops the piece if necessary.
+ * - Updates the game board with the placed piece.
+ * - Resets the piece to its initial position if it cannot move downward.
+ */
+const handlePiecePlacement = () => {
   if (piece.position.y >= BOARD_HEIGHT - piece.shape.length || !isValidMove('ArrowDown')) {
     piece.shape.forEach((row, y) => {
       row.forEach((block, x) => {
-        BOARD[piece.position.y + y][piece.position.x + x] = 1;
+        if (piece.shape[y][x] === 1) {
+          BOARD[piece.position.y + y][piece.position.x + x] = 1;
+        }
       });
     });
 
-    piece.position = { y: -1, x: 4 };
+    generateNewPiece();
   } else {
     //piece.position.y = piece.position.y + 1;
   }
 };
 
-const cleanRow = () => {
+/**
+ * Clears completed rows on the Tetris game board:
+ * - Iterates through each row to check if it's filled with blocks.
+ * - Removes filled rows by shifting rows above them downward.
+ */
+const clearRow = () => {
   for (let row = 1; row < BOARD_HEIGHT; row++) {
     if (!BOARD[row].some((elem) => elem === 0)) {
       BOARD[row] = BOARD[row - 1];
     }
   }
-}
+};
+
+/**
+ * Generates a new random piece and sets its initial position.
+ */
+const generateNewPiece = () => {
+  piece.position = { y: -1, x: 4 };
+  piece.shape = shapes[Math.floor(Math.random() * 4)];
+};
 
 document.addEventListener('keydown', (e) => {
-  const bottomLimit = piece.position.y < BOARD_HEIGHT - piece.shape.length;
-  const rightLimit = piece.position.x < BOARD_WIDTH - piece.shape[0].length;
-  const leftLimit = piece.position.x > 0;
-
   if (e.key === 'ArrowLeft' && leftLimit && isValidMove('ArrowLeft')) {
     piece.position.x--;
   } else if (e.key === 'ArrowRight' && rightLimit && isValidMove('ArrowRight')) {
